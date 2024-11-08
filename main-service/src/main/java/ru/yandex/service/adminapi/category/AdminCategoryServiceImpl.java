487ad7f8149bb9688e37a670f804233bf21643eb
Evgeny <kharchenko.e.v@yandex.ru>
2024-11-08 11:14:37 +0300

    Revert "style: code style fixes"
    This reverts commit 734de566519162ad016b91d4b2922d45f43e5563.

--- main-service/src/main/java/ru/yandex/service/adminapi/category/AdminCategoryServiceImpl.java
+++ main-service/src/main/java/ru/yandex/service/adminapi/category/AdminCategoryServiceImpl.java
@@ -37,8 +37,7 @@ public class AdminCategoryServiceImpl implements AdminCategoryService {
             throw new ConflictException(e.getMessage() + "\nFailed to save category in adminCategoryService");
         }
         //TODO check
-        log.info(MessageFormat
-                .format("Added category - {0}", category.getName()));
+        log.info(MessageFormat.format("Added category - {0}", category.getName()));
 
         return category;
     }
@@ -54,16 +53,15 @@ public class AdminCategoryServiceImpl implements AdminCategoryService {
             repository.deleteById(categoryId);
         }
 
-        log.info(MessageFormat
-                .format("Category (name = {0}) was deleted", category.getName()));
+        log.info(MessageFormat.format("Category (name = {0}) was deleted", category.getName()));
     }
 
     @Override
     @Transactional
     public Category updateCategoryById(long categoryId, NewCategoryDto newCategoryDto) {
         Category category = repository.findById(categoryId).orElseThrow(()
-                -> new NotFoundException(MessageFormat
-                .format("Category with id='{}'{0} was not found", categoryId)));
+                -> new NotFoundException(MessageFormat.
+                format("Category with id='{}'{0} was not found", categoryId)));
         //FIXME
         /*if (newCategoryDto.getName().length() > 50) {
             throw new IncorrectParameterException("Field too long (more than 50 symbols)");
@@ -76,8 +74,7 @@ public class AdminCategoryServiceImpl implements AdminCategoryService {
             throw new ConflictException("Failed to save category in adminCategoryService");
         }
 
-        log.info(MessageFormat
-                .format("Changed name {0} to {1}", category.getId(), category.getName()));
+        log.info(MessageFormat.format("Changed name {0} to {1}", category.getId(), category.getName()));
 
         return category;
     }
