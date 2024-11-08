a7518d75465f7879b0704979225fd3bf9f24f589
Evgeny <kharchenko.e.v@yandex.ru>
2024-11-08 09:45:10 +0300

    fix: fix AdminCategoryServiceImpl

--- main-service/src/main/java/ru/yandex/dto/category/NewCategoryDto.java
+++ main-service/src/main/java/ru/yandex/dto/category/NewCategoryDto.java
@@ -3,6 +3,7 @@ package ru.yandex.dto.category;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +16,7 @@ public class NewCategoryDto {
     @NotBlank
     @NotNull
     @NotEmpty
+    @Size(max = 50)
     private String name;
 
 }
--- main-service/src/main/java/ru/yandex/service/adminapi/category/AdminCategoryServiceImpl.java
+++ main-service/src/main/java/ru/yandex/service/adminapi/category/AdminCategoryServiceImpl.java
@@ -1,19 +1,19 @@
 package ru.yandex.service.adminapi.category;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 import ru.yandex.dto.category.NewCategoryDto;
 import ru.yandex.error.apierror.exceptions.ConflictException;
-import ru.yandex.error.apierror.exceptions.IncorrectParameterException;
 import ru.yandex.error.apierror.exceptions.NotFoundException;
 import ru.yandex.mapper.CategoryMapper;
 import ru.yandex.model.category.Category;
 import ru.yandex.repository.CategoryRepository;
 import ru.yandex.repository.EventRepository;
+
+import java.text.MessageFormat;
 
 @Service
 @RequiredArgsConstructor
@@ -24,24 +24,26 @@ public class AdminCategoryServiceImpl implements AdminCategoryService {
     private final CategoryMapper mapper;
 
     @Override
+    @Transactional
     public Category addCategory(NewCategoryDto newCategoryDto) {
         Category category;
-        if (newCategoryDto.getName().length() > 50) {
+        /*if (newCategoryDto.getName().length() > 50) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Category name is too long!");
-        }
+        }*/
 
         try {
             category = repository.save(mapper.toCategoryFromNewCategoryDto(newCategoryDto));
         } catch (DataIntegrityViolationException e) {
-            throw new ConflictException("Failed to save category in adminCategoryService");
+            throw new ConflictException(e.getMessage() + "\nFailed to save category in adminCategoryService");
         }
-
-        log.info("Added category - " + category.getName());
+        //TODO check
+        log.info(MessageFormat.format("Added category - {0}", category.getName()));
 
         return category;
     }
 
     @Override
+    @Transactional
     public void deleteCategoryById(long categoryId) {
         Category category = repository.findById(categoryId).orElseThrow();
 
@@ -51,17 +53,19 @@ public class AdminCategoryServiceImpl implements AdminCategoryService {
             repository.deleteById(categoryId);
         }
 
-        log.info("Category (name = " + category.getName() + ") was deleted");
+        log.info(MessageFormat.format("Category (name = {0}) was deleted", category.getName()));
     }
 
     @Override
+    @Transactional
     public Category updateCategoryById(long categoryId, NewCategoryDto newCategoryDto) {
         Category category = repository.findById(categoryId).orElseThrow(()
-                -> new NotFoundException("Category with id=" + categoryId + " was not found"));
-
-        if (newCategoryDto.getName().length() > 50) {
+                -> new NotFoundException(MessageFormat.
+                format("Category with id='{}'{0} was not found", categoryId)));
+        //FIXME
+        /*if (newCategoryDto.getName().length() > 50) {
             throw new IncorrectParameterException("Field too long (more than 50 symbols)");
-        }
+        }*/
 
         try {
             category.setName(newCategoryDto.getName());
@@ -70,7 +74,7 @@ public class AdminCategoryServiceImpl implements AdminCategoryService {
             throw new ConflictException("Failed to save category in adminCategoryService");
         }
 
-        log.info("Changed name " + category.getId() + " to " + category.getName());
+        log.info(MessageFormat.format("Changed name {0} to {1}", category.getId(), category.getName()));
 
         return category;
     }
